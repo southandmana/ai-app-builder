dc9f5ea4c986f668c9c7940645bd1ff085f46a7e
southerncoromandel <scoromandel.chatsling@gmail.com>
2025-09-03 15:47:53 +1200

    Fix import issues by adding __init__.py to tools and adjusting test imports

--- tests/test_chat_interface_unittest.py
+++ tests/test_chat_interface_unittest.py
@@ -1,5 +1,9 @@
 import unittest
 import signal
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../tools')))
 from cli_interface import chat_interface
 
 
